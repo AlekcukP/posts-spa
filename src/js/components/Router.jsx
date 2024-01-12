b1c4f786d4d63158fa2d520468ceaa111d3f0e38
Oleksii Pokhyl <alekcukp@gmail.com>
2024-01-12 17:34:17 +0200

    Changed browser router component to hash router

--- src/js/components/Router.jsx
+++ src/js/components/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import DashboardTemplate from '../templates/DashboardTemplate';
 import HomePage from '../pages/HomePage';
 import UsersPage from '../pages/UsersPage';
@@ -8,7 +8,7 @@ import PostsPage from '../pages/PostsPage';
 
 const Router = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path='/' exact element={<HomePage />} />
 
@@ -25,7 +25,7 @@ const Router = () => {
                 </Route>
                 <Route path='*' element={<div>Not Found</div>} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
